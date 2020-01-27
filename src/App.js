fbecccb5715445a2c83adbadd4d52d6320ba8d93
Tex Andersen <tex.andersen@gmail.com>
2020-01-27 13:20:22 +1000

    start fresh from latest create react app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import Warrior from './warrior'
-
-const styles = {
-  root: {
-    height: '100vh',
-    width: '100vw',
-  }
-};
-
-const App = () => (
-  <div style={styles.root}>
-    <Warrior />
-  </div>
-);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
--- src/reducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import warrior from './warrior/duck';
-
-const rootReducer = combineReducers({warrior});
-
-export default rootReducer;
